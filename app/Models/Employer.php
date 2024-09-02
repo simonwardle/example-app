0b38ee70484ca5bcdbd7203374412f8834639215
simonwardle <simon.wardle@outlook.com>
2024-09-02 19:37:25 +0100

    Completed Day 11

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Employer extends Model
 {
     use HasFactory;
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
