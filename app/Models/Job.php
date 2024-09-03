8cb749a09091b0f51294dc21cbed64b7e1e48143
simonwardle <simon.wardle@outlook.com>
2024-09-03 20:29:53 +0100

    finished day 13

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -16,4 +16,11 @@ class Job extends Model
         return $this->belongsTo(Employer::class);
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey:'job_listing_id');
+    }
+
+
+
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+
     use HasFactory;
+
+    public function jobs()
+    {
+        return $this->belongsToMany(Job::class, relatedPivotKey:'job_listing_id');
+    }
 }
