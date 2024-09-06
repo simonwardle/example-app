9929cf44bedbe0613eeb25792e20b1c10f4c86b0
simonwardle <simon.wardle@outlook.com>
2024-09-06 13:07:27 +0100

    Working on Forms

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,22 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->paginate(5);
-    
-    return view('jobs', [
+
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
